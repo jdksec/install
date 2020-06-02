4b7a3523c45950e2fb86000015639b41fef82a56
jdksec <44243011+jdksec@users.noreply.github.com>
2020-06-02 10:40:05 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,25 @@
+# Basic tool installation
+
+apt-get -y update
+apt-get -y upgrade
+apt-get -y dist-upgrade
+apt-get -y install curl
+apt-get -y install git
+apt-get -y install net-tools
+apt-get -y install zsh
+apt-get -y install vim
+
+# Docker install
+apt-get remove docker docker-engine docker.io containerd runc
+apt-get install apt-transport-https ca-certificates gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io
+
+curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+
+# Install ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
