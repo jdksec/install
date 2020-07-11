d7123b37f284e1294b5f3bb5f9c286aa7bc84909
jdksec <44243011+jdksec@users.noreply.github.com>
2020-07-11 18:57:24 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -79,8 +79,6 @@ apt-get install docker-ce docker-ce-cli containerd.io
 curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-# Install ohmyzsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.zshrc
 echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
 
