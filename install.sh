1f2986d3d851f4e83f029da07b49f44142872cf1
jdksec <44243011+jdksec@users.noreply.github.com>
2021-10-24 06:20:24 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -166,7 +166,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
 sudo apt install docker-ce -ysource
-sudo systemctl status docker
 sudo systemctl disable docker
 sudo usermod -aG docker ${USER}
 
