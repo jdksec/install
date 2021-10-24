2562ce32b3db97f19f83bbaead7ba67473056de2
jdksec <44243011+jdksec@users.noreply.github.com>
2021-10-24 06:03:49 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -264,6 +264,13 @@ git clone https://github.com/projectdiscovery/nuclei-templates.git
 git clone https://github.com/disclose/disclose.git
 git clone https://github.com/projectdiscovery/public-bugbounty-programs.git
 
+# sublime
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text
+
 sudo systemctl disable apache2 
 sudo systemctl stop apache2
 
