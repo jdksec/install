392cdb35b43732ba2d4d7d1bde3fb0d015c3e316
jdksec <44243011+jdksec@users.noreply.github.com>
2021-10-24 07:54:58 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -165,19 +165,9 @@ sudo apt install apt-transport-https ca-certificates curl software-properties-co
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
-sudo apt install docker-ce -y
+sudo apt install docker-ce docker-compose -y
 sudo systemctl disable docker
 sudo usermod -aG docker ${USER}
-
-#apt-get remove docker docker-engine docker.io containerd runc
-#apt-get install apt-transport-https ca-certificates gnupg-agent software-properties-common
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-#add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-#apt-get update
-#apt-get install docker-ce docker-ce-cli containerd.io
-
-#curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-#sudo chmod +x /usr/local/bin/docker-compose
 
 # Dropbox install
 #cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
