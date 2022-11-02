96853de89f88fc0a142c02b49a826f795a94dd9f
jdksec <44243011+jdksec@users.noreply.github.com>
2022-11-02 08:25:43 +0000

    Update install-small.sh

--- install-small.sh
+++ install-small.sh
@@ -10,23 +10,11 @@ wget https://raw.githubusercontent.com/jdksec/install/master/.vimrc -O ~/.vimrc
 wget https://raw.githubusercontent.com/jdksec/install/master/.tmux.conf -O ~/.tmux.conf
 wget https://raw.githubusercontent.com/jdksec/install/master/.zprofile -O ~/.zprofile
 
-# Docker install
-#sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-#apt-cache policy docker-ce
-#sudo apt install docker-ce docker-compose -y
-#sudo systemctl disable docker
-#sudo usermod -aG docker ${USER}
-
 # Create folders
 cd ~/
 mkdir .trash
 mkdir Tools
 mkdir Lab
 
-sudo systemctl disable apache2
-sudo systemctl stop apache2
 chsh -s $(which zsh)
 zsh
-#sudo reboot
