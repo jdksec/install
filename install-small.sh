495c2531bfb1a0d5fefceb87c1e6f18256269c3c
jdksec <44243011+jdksec@users.noreply.github.com>
2022-11-02 08:24:30 +0000

    Update install-small.sh

--- install-small.sh
+++ install-small.sh
@@ -1,9 +1,5 @@
 # Basic tool installation
 
-#sudo visudo
-
-#sudo chmod 777 bins/*
-
 # These are not all on one line as sometimes a package wont install interupting the whole install process.
 sudo apt-get -y update
 sudo apt-get -y upgrade
@@ -15,13 +11,13 @@ wget https://raw.githubusercontent.com/jdksec/install/master/.tmux.conf -O ~/.tm
 wget https://raw.githubusercontent.com/jdksec/install/master/.zprofile -O ~/.zprofile
 
 # Docker install
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-apt-cache policy docker-ce
-sudo apt install docker-ce docker-compose -y
-sudo systemctl disable docker
-sudo usermod -aG docker ${USER}
+#sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+#apt-cache policy docker-ce
+#sudo apt install docker-ce docker-compose -y
+#sudo systemctl disable docker
+#sudo usermod -aG docker ${USER}
 
 # Create folders
 cd ~/
@@ -32,4 +28,5 @@ mkdir Lab
 sudo systemctl disable apache2
 sudo systemctl stop apache2
 chsh -s $(which zsh)
+zsh
 #sudo reboot
