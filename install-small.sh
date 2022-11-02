cd351274ff2dd6f6540874e66c282bc067de93b6
jdksec <44243011+jdksec@users.noreply.github.com>
2022-11-02 08:15:36 +0000

    Update install-small.sh

--- install-small.sh
+++ install-small.sh
@@ -2,21 +2,17 @@
 
 #sudo visudo
 
-sudo chmod 777 bins/*
+#sudo chmod 777 bins/*
 
 # These are not all on one line as sometimes a package wont install interupting the whole install process.
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y dist-upgrade
-
-# Create dotfiles
-cd ~/install
-cat .vimrc > ~/.vimrc
-cat .tmux.conf > ~/.tmux.conf
-cat .zprofile > ~/.zprofile
-cat .zprofile > ~/.zprofile
-cat .zshrc > ~/.zshrc
-cp jdksec.zsh-theme ~/.oh-my-zsh/themes/
+sudo apt install zsh git tmux
+wget https://raw.githubusercontent.com/jdksec/install/master/.zshrc -O ~/.zshrc
+wget https://raw.githubusercontent.com/jdksec/install/master/.vimrc -O ~/.vimrc
+wget https://raw.githubusercontent.com/jdksec/install/master/.tmux.conf -O ~/.tmux.conf
+wget https://raw.githubusercontent.com/jdksec/install/master/.zprofile -O ~/.zprofile
 
 # Docker install
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
@@ -36,5 +32,4 @@ mkdir Lab
 sudo systemctl disable apache2
 sudo systemctl stop apache2
 
-echo "Now run: source ~/.zshrc"
 sudo reboot
