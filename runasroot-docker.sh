186c5025ecfa457907d7e1f9abacefc0ae0c1eb0
jdksec <44243011+jdksec@users.noreply.github.com>
2022-11-09 07:19:40 +0000

    Create runasroot-docker.sh

---
+++ runasroot-docker.sh
@@ -0,0 +1,10 @@
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo echo 'deb [arch=amd64] https://download.docker.com/linux/debian buster stable' > /etc/apt/sources.list.d/docker.list
+sudo apt-get update -y
+sudo apt-get remove docker docker-engine docker.io docker-compose docker-ce -y
+sudo apt-get -y install docker-ce
+sudo apt-get -y install docker-compose
+sudo service docker status
+echo "Run the following as the current user"
+echo "sudo groupadd docker"
+echo "sudo usermod -aG docker \$USER"
