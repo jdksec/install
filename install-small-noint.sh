19f4a30bb62ecae434c5a4d6be6fc9c738d39e02
jdksec <44243011+jdksec@users.noreply.github.com>
2023-05-05 08:32:17 +0100

    Update install-small-noint.sh

--- install-small-noint.sh
+++ install-small-noint.sh
@@ -42,3 +42,14 @@ git clone https://github.com/CiscoCXSecurity/enum4linux.git
 git clone https://github.com/drwetter/testssl.sh.git
 chmod +x testssl.sh/testssl.sh
 cd ~/
+
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo echo 'deb [arch=amd64] https://download.docker.com/linux/debian buster stable' > /etc/apt/sources.list.d/docker.list
+sudo apt-get update -y
+sudo apt-get remove docker docker-engine docker.io docker-compose docker-ce -y
+sudo apt-get -y install docker-ce
+sudo apt-get -y install docker-compose
+sudo service docker status
+echo "Run the following as the current user"
+echo "sudo groupadd docker; sudo usermod -aG docker \$USER"
+echo "Reboot or log out and backin to get permissions"
